e86957c4597d14f7c0eec48cbde2381eba2deff4
Vneb23 <benjamin.voisin@lilo.org>
2022-10-10 18:08:29 +0200

    Lecture du Makefile, question 5 bien bien

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "regle.h"
+#include "ens_regles.h"
 
 /*
  *             ///((/////                   *&&&&&&&&              %&&&&&&&&    
@@ -22,7 +23,18 @@
  *   ////////                                                        |_|   |_|  \___/|_| |_|_|_|\___/     |____|\___/____|____|
 */
 
-void lire(void){
+int nombre_regles(FILE *fichier){
+	int i = 0;
+	int c;
+	while((c = fgetc(fichier)) != EOF){
+		i += c == ':';
+	}
+	fseek(fichier,0, SEEK_SET);
+	return i;
+}
+
+
+ens_regles * lire(void){
 	FILE * fichier;
 	char * ligne = NULL;
 	size_t len = 0;
@@ -30,21 +42,57 @@ void lire(void){
 	if (fichier == NULL){
 		fprintf(stderr,"Le fichier est introuvable");
 	}
+
+	int nb_regles = nombre_regles(fichier);
+	ens_regles * ens = nouvel_ensemble(nb_regles);
+	long debut = ftell(fichier);
+
 	while(getline(&ligne, &len, fichier) != -1){
 		if (ligne[0] != '\t'){
-			char *coupe = strtok(ligne, " ");
-			int nb_mots = strlen(coupe);
-			for (int i=0; i < nb_mots ; i++){
-				printf("%c",coupe[i]);
+			char * nom_regle  = strtok(ligne, ":");
+
+			char *prerequis;
+			int nb_prerequis = 0;
+			while((prerequis = strtok(NULL, " ")) != 0){
+				nb_prerequis ++;
 			}
-		};
+
+			int nb_commandes = 0;
+			while(getline(&ligne, &len, fichier) != -1){
+				if(ligne[0] != '\t'){
+					break;
+				}
+				nb_commandes += 1;
+			}
+
+			fseek(fichier,debut,SEEK_SET);
+			regle * r = nouvelle_regle(nom_regle,nb_prerequis,nb_commandes);
+
+
+			while((prerequis = strtok(NULL, " ")) != 0){
+				ajouter_prerequis(r, prerequis);
+			}
+
+
+			while(getline(&ligne, &len, fichier) != -1){
+				if(ligne[0] != '\t'){
+					break;
+				}
+				ligne += 1;
+				ajouter_commande(r,ligne);
+			}
+
+			ajouter_regle(ens, r);
+			
+
+		}
+		debut = ftell(fichier);
 	};
-	free(ligne);
 	fclose(fichier);
+	return ens;
 }
 
 int main(void) {
-	printf("Hello, World !\n");
 	lire();
 
 	return 0;
