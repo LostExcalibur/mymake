56161d1c7c9032980c107a9c80f30b3b3a7f1006
LostExcalibur <romaindebeaucorps@yahoo.fr>
2022-10-10 16:48:55 +0200

    Fin de la question 3

--- ens_regles.c
+++ ens_regles.c
@@ -1,1 +1,38 @@
 #include "ens_regles.h"
+#include "regle.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+ens_regles* nouvel_ensemble(int taille) {
+	ens_regles* ens = malloc(sizeof(ens_regles));
+
+	ens->regles = malloc(taille * sizeof(regle *));
+
+	ens->regle_actuelle = 0;
+
+	return ens;
+}
+
+void detruire_ensemble(ens_regles* ens) {
+	free(ens->regles);
+
+	free(ens);
+}
+
+void ajouter_regle(ens_regles* ens, regle* r) {
+	ens->regles[ens->regle_actuelle] = r;
+	ens->regle_actuelle++;
+}
+
+
+// Renvoit un pointeur vers la règle ayant le nom correspondant.
+// Renvoit NULL si elle n'existe pas dans l'ensemble
+regle* trouver_regle(ens_regles* ens, char* nom) {
+	for (int i = 0; i < ens->regle_actuelle; i++) {
+		if (strcmp(ens->regles[i]->nom, nom) == 0) {
+			return ens->regles[i];
+		}		
+	}
+	return NULL;
+}	
--- ens_regles.h
+++ ens_regles.h
@@ -6,8 +6,13 @@
 typedef struct Ens_Regles {
 	regle** regles;
 
-	int n_regles;
+	int regle_actuelle;
 } ens_regles;
 
+
+ens_regles* nouvel_ensemble(int taille);
+void detruire_ensemble(ens_regles* ens);
+void ajouter_regle(ens_regles* ens, regle* r);
+regle* trouver_regle(ens_regles* ens, char* nom);
 
 #endif // __ENS_REGLES_H
